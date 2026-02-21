79dd92e19ee97675f674637d7cba376935383b20
sravani <ravulasravani08@gmail.com>
2026-02-21 23:11:22 +0530

    backend logic for logginf and authentication using multiple interfaces and class

--- backendserviceInterface.java
+++ backendserviceInterface.java
@@ -2,24 +2,24 @@
 interface 1 = logger wants to login , interface 2 = authenticator  
 this  both is used in one main class 
 */
-import java.util.Scanner;
+import java.util.Scanner; //a scanner package for taking userinput--
 
-interface Login{
+interface Login{ //1st interface for email login page 
     void logger();
 
 }
-interface authenticate{
+interface authenticate{ //2nd interface for password setup and and verifing the password
     void authenticator();
 }
-class webServer implements Login , authenticate{
+class webServer implements Login , authenticate{//creating a class using multiple interfaces 
     private String EmailID;
     private String PassWord;
     private Scanner sc = new Scanner(System.in);
-    /*public webServer (String email , String password) {
+    /*public webServer (String email , String password) {//wanted to use but realised that using scanner you can skip this constructor
         this.EmailID = email;
         this.PassWord = password;*/
     @Override
-    public void logger(){
+    public void logger(){ //email login page 
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the email ID: ");
         String email = sc.nextLine();
@@ -30,7 +30,7 @@ class webServer implements Login , authenticate{
     }
 
     @Override
-    public void authenticator(){
+    public void authenticator(){ //password setup page 
         Scanner sc = new Scanner(System.in);
         System.out.println("Eneter your secret password ");
         String password = sc.nextLine();
@@ -46,11 +46,11 @@ class webServer implements Login , authenticate{
     }
 
 }
-public class backendserviceInterface{
+public class backendserviceInterface{//main class
     public static void main(String[] args) {
-        webServer wb = new webServer();
+        webServer wb = new webServer(); //creating object for class and abstract methods 
         boolean check = false;
-        if (check){
+        if (check){//choosed to do any one abstract method
             wb.logger();
         }else{
             wb.authenticator();
